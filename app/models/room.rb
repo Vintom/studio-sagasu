9e16809eb3f5bd4579d86140694baab0ecd8d5fd
ryom-a <ryoma.shikami@gmail.com>
2018-11-20 12:17:58 +0900

    Add validation on columns in room model

--- app/models/room.rb
+++ app/models/room.rb
@@ -3,6 +3,14 @@ class Room < ApplicationRecord
   belongs_to :studio
 
   validates :capacity, presence: true, numericality: { only_integer: true, grater_than: 0 }
+  validates :price, presence: true, numericality: { only_integer: true, grater_than: 0 }
+  validates :speaker, presence: true
+  validates :mixer, presence: true
+  validates :cd, presence: true
+  validates :md, presence: true
+  validates :mp3, presence: true
+  validates :dimmable, presence: true
+  validates :wifi, presence: true
 
   scope :displayed, -> { where(status: Room.statuses[:active]) }
   scope :by_studio, ->(studio_id) { where(studio_id: studio_id) if studio_id.present?}
