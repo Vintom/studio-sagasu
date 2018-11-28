277c57e218198a7deae9597b6c66870ae60f35a6
ryom-a <ryoma.shikami@gmail.com>
2018-11-28 18:40:52 +0900

    Insert initial records into search_people table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+SearchPerson.create(:name =>'~5人', :min => 1, :max => 5)
+SearchPerson.create(:name =>'6~10人', :min => 6, :max => 10)
+SearchPerson.create(:name =>'11~20人', :min => 11, :max => 20)
+SearchPerson.create(:name =>'21~30人', :min => 21, :max => 30)
+SearchPerson.create(:name =>'31人~', :min => 31, :max => 99999)
+SearchPerson.create(:name =>'指定しない', :min => 1, :max => 99999)
