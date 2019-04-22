3cacb5f5bf97035fe8078beedd8092ce5be117f4
ryom-a <ryoma.shikami@gmail.com>
2019-04-22 17:58:41 +0900

    Add instance variable

--- app/mailers/report_mailer.rb
+++ app/mailers/report_mailer.rb
@@ -2,6 +2,9 @@ class ReportMailer < ApplicationMailer
   default from: "noreply@vintom.co.jp"
 
   def send_studios_report
+    @active_studio = Studio.where(status: 1)
+    @inactive_studio = Studio.where(status: 0)
+    @reviewing_studio = Studio.where(status: 2)
     mail(
       subject: "sagasu.spaceスタジオ数週次レポート"
       to: "all@vintom.co.jp"
