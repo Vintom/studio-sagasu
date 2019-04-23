f8eabce277cd963dc957e35155b9263166c90f78
ryom-a <ryoma.shikami@gmail.com>
2019-04-23 23:39:01 +0900

    Modify time and stg mailer setting

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,8 +1,19 @@
 if Rails.env.production?
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
-    address: 'smtp.gmail.com',
-    domain: 'gmail.com',
+    address: 'everysunday.sakura.ne.jp',
+    domain: 'vintom.co.jp',
+    port: 587,
+    user_name: 'noreply@vintom.co.jp',
+    password: 'rep-?Pass-nono',
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
+elsif Rails.env.staging?
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    address: 'everysunday.sakura.ne.jp',
+    domain: 'vintom.co.jp',
     port: 587,
     user_name: 'noreply@vintom.co.jp',
     password: 'rep-?Pass-nono',
