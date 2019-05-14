f13b2e36315b4026596c9ded8a782bf72bb13584
ryom-a <ryoma.shikami@gmail.com>
2019-05-14 20:29:32 +0900

    Made a basic table

--- app/mailers/report_mailer.rb
+++ app/mailers/report_mailer.rb
@@ -2,9 +2,16 @@ class ReportMailer < ApplicationMailer
   default from: "noreply@vintom.co.jp"
 
   def send_studios_report
-    @active_studio = Studio.where(status: 1)
-    @inactive_studio = Studio.where(status: 0)
-    @reviewing_studio = Studio.where(status: 2)
+    @area_studios = Studio.includes(:area)
+    @studio_rooms = Room.includes(:studio)
+    @area_studio_rooms = Room.includes(studio: :area)
+    @prefectures = Area.order(:id).group(:prefecture).pluck(:prefecture)
+    @active_studios = Studio.where(status: 1)
+    @inactive_studios = Studio.where(status: 0)
+    @reviewing_studios = Studio.where(status: 2)
+    @active_rooms = Room.where(status: 1)
+    @inactive_rooms = Room.where(status: 0)
+    @reviewing_rooms = Room.where(status: 2)
     mail(
       subject: "sagasu.spaceスタジオ数週次レポート",
       to: "all@vintom.co.jp"
