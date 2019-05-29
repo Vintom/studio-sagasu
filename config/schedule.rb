802f12da976e817fb6b6d27c279587266c6e1b02
ryom-a <ryoma.shikami@gmail.com>
2019-05-29 19:54:36 +0900

    Set schedule of rake task

--- config/schedule.rb
+++ config/schedule.rb
@@ -7,4 +7,9 @@ if @environment.to_sym == :production
   every :tuesday, at: '9:00 pm' do
     rake "send_email:number_of_studios"
   end
+  # 火曜日のpm9時1分にスケジューリング
+  every :tuesday, at: '9:01 pm' do
+    rake "update_last_week_status:studios"
+    rake "update_last_week_status:rooms"
+  end
 end
