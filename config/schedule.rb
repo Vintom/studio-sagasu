43e11a127de73c4c0aaaeb9baabfe14a8a770bdc
ryom-a <ryoma.shikami@gmail.com>
2019-05-29 19:58:42 +0900

    Added comments

--- config/schedule.rb
+++ config/schedule.rb
@@ -4,10 +4,12 @@ set :output, 'log/cron.log'
 # production環境のジョブ指定
 if @environment.to_sym == :production
   # 火曜日のpm9時にスケジューリング
+  # スタジオ数/ルーム数の社内メール通知
   every :tuesday, at: '9:00 pm' do
     rake "send_email:number_of_studios"
   end
   # 火曜日のpm9時1分にスケジューリング
+  # studios/roomsテーブルのlast_week_statusカラムをUPDATE
   every :tuesday, at: '9:01 pm' do
     rake "update_last_week_status:studios"
     rake "update_last_week_status:rooms"
